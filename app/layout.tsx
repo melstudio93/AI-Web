c39545f32dd774c628f0aa96adae4da1c5e7847c
melstudio93 <143149939+melstudio93@users.noreply.github.com>
2023-09-05 09:01:17 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { getClientConfig } from "./config/client";
 import { type Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "MajorBio AI Assistant",
+  description: "Your personal ChatGPT AI assistant.",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "MajorBio AI Assistant",
     statusBarStyle: "default",
   },
 };
