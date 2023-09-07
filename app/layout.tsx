ee94daa946ed56c2978f1fdf917f665c71d2b91c
melstudio93 <143149939+melstudio93@users.noreply.github.com>
2023-09-08 02:06:18 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { type Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "MajorBio AI Assistant",
-  description: "Your personal ChatGPT AI assistant.",
+  description: "Your personal ChatGPT AI Assistant.",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -16,13 +16,17 @@ export const metadata: Metadata = {
   themeColor: [
     { media: "(prefers-color-scheme: light)", color: "#fafafa" },
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
+    { media: "(prefers-color-scheme: amour)", color: "#fafafa" },
+    { media: "(prefers-color-scheme: hubspot)", color: "#151515" },
+    { media: "(prefers-color-scheme: authentic)", color: "#fafafa" },
+    { media: "(prefers-color-scheme: tiktok)", color: "#151515" },
   ],
   appleWebApp: {
     title: "MajorBio AI Assistant",
     statusBarStyle: "default",
   },
 };
-
+ 
 export default function RootLayout({
   children,
 }: {
